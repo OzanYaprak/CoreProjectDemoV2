ef303a0da9a6d30962f71ad70ce7b4ee3e4a811a
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2023-08-25 18:42:38 +0300

    Final

---
+++ BusinessLayer/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.CustomerName).NotEmpty().WithMessage("Müşteri adı boş geçilemez");
+            RuleFor(x => x.CustomerCity).NotEmpty().WithMessage("Müşteri şehir bilgisi geçilemez");
+        }
+    }
+}
---
+++ BusinessLayer/FluentValidation/ProductValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator() 
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün adı boş geçilemez");
+        }
+    }
+}
